75eb3bb262ebc21084f7a4a2ee6b21434423ec9c
Kemran <109353270+remr2005@users.noreply.github.com>
2023-12-02 18:45:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	r.HandleFunc("/PersonDelete/git", deletePerson_git).Methods("GET")
+	r.HandleFunc("/PersonDelete/tel", deletePerson_tel).Methods("GET")
+	r.HandleFunc("/changeVar/git", changeVars_git).Methods("GET")
+	r.HandleFunc("/changeVar/tel", changeVars_tel).Methods("GET")
 	r.HandleFunc("/getPersons", getMENSCHEN).Methods("GET")
 	r.HandleFunc("/isPersonExist/git", isPersonExist_Git).Methods("GET")
 	r.HandleFunc("/isPersonExist/tel", isPersonExist_Tel).Methods("GET")
