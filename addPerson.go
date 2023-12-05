76325ae790d3d111c3b88e2db8962e7a6767b4ec
Kemran <109353270+remr2005@users.noreply.github.com>
2023-12-06 00:37:34 +0300

    Update addPerson.go

--- addPerson.go
+++ addPerson.go
@@ -3,22 +3,12 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
-// Person структура представляет пользователя
-type Person struct {
-	SURNAME     string `json:"SURNAME"`
-	NAMEP       string `json:"NAMEP"`
-	FATHER_NAME string `json:"FATHER_NAME"`
-	GITID       string `json:"GITID"`
-	TELID       string `json:"TELID"`
-}
-
 func addPerson(w http.ResponseWriter, r *http.Request) {
 	// Парсинг JSON-данных из тела запроса в структуру Person
-	var newPerson Person
+	var newPerson person
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&newPerson); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
@@ -34,12 +24,11 @@ func addPerson(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	// Выполнение SQL-запроса для добавления нового пользователя
-	result, err := db.Exec("INSERT INTO persons.MENSCHEN (SURNAME, NAMEP, FATHER_NAME, GITID, TELID) VALUES (?, ?, ?, ?)",
-		newPerson.SURNAME, newPerson.NAMEP, newPerson.FATHER_NAME, newPerson.GITID, newPerson.TELID)
+	result, err := db.Exec("INSERT INTO persons.MENSCHEN (GITID, TELID, SURNAME, NAMEP, FATHER_NAME, GROUPP, STUDENT, LEHRER, ADMINP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		newPerson.GITID, newPerson.TELID, newPerson.SURNAME, newPerson.NAMEP, newPerson.FATHER_NAME, newPerson.GROUPP, newPerson.STUDENT, newPerson.LEHRER, newPerson.ADMINP)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(result.LastInsertId()) // ID последнего добавленного объекта
-	fmt.Println(result.RowsAffected()) // Количество затронутых строк
+	result = result
 }
